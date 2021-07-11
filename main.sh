7fdfb07a0f32366c00077c86c6ad696b8459edc5
BuddhikaD <buddhikadananjaya55@gmail.com>
2021-07-12 00:52:30 +0530

    Project initiate

---
+++ main.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+########################################################
+# Author: Buddhika Dananjaya                           #
+# Info: Script for universal linux distro updater      #
+# License: MIT (c)                                     #
+########################################################
+
+
+GREEN="32"
+BOLDGREEN="\e[1;${GREEN}m"
+ENDCOLOR="\e[0m"
+
+printf """${BOLDGREEN}                        
+Author: @BuddhikaD
+Url: https://github.com/BuddhikaD/Universal-Update-Script
+"""
