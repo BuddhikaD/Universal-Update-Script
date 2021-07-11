d0a3f6bee96f4a22731c22403de33fa5b46cf566
BuddhikaD <buddhikadananjaya55@gmail.com>
2021-07-12 00:53:03 +0530

    check if the pkg manager is arch based

--- main.sh
+++ main.sh
@@ -15,3 +15,11 @@ printf """${BOLDGREEN}
 Author: @BuddhikaD
 Url: https://github.com/BuddhikaD/Universal-Update-Script
 """
+cd /etc
+## Test if the local host is Arch based
+
+if  [ -d /etc/pacman.d ]
+then
+    ## Run the Arch version of the update command
+    sudo pacman -Syu
+fi
